dbbe8b3896dfd0329cb498cc97e1ee00f2f49c68
Niraj Kulkarni <nirajkulkarni2609@gmail.com>
2025-04-02 16:55:18 +0100

    working simulate_transfer_coin

--- examples/simulate_transfer_coin.py
+++ examples/simulate_transfer_coin.py
@@ -19,26 +19,26 @@ from .common import FAUCET_AUTH_TOKEN, FAUCET_URL, NODE_URL
 
 async def main():
     rest_client = RestClient(NODE_URL)
-    faucet_client = FaucetClient(
-        FAUCET_URL, rest_client, FAUCET_AUTH_TOKEN
-    )  # <:!:section_1
+    # faucet_client = FaucetClient(
+    #     FAUCET_URL, rest_client, FAUCET_AUTH_TOKEN
+    # )  # <:!:section_1
 
-    alice = Account.generate()
-    bob = Account.generate()
+    alice =  Account.load_key("0x68e7f9a75606e0baa3c2487b32ba016ead6d8d71a220f5a69a8b7645468c54df")
+    bob =  Account.generate()
 
     print("\n=== Addresses ===")
     print(f"Alice: {alice.address()}")
     print(f"Bob: {bob.address()}")
 
-    await faucet_client.fund_account(alice.address(), 100_000_000)
+    # await faucet_client.fund_account(alice.address(), 100_000_000)
 
     payload = EntryFunction.natural(
-        "0x1::coin",
+        "0x1::endless_coin",
         "transfer",
-        [TypeTag(StructTag.from_str("0x1::aptos_coin::AptosCoin"))],
-        [
+        [],
+        [   
             TransactionArgument(bob.address(), Serializer.struct),
-            TransactionArgument(100_000, Serializer.u64),
+            TransactionArgument(100_000, Serializer.u128),
         ],
     )
     transaction = await rest_client.create_bcs_transaction(
@@ -47,11 +47,15 @@ async def main():
 
     print("\n=== Simulate before creating Bob's Account ===")
     output = await rest_client.simulate_transaction(transaction, alice)
-    assert output[0]["vm_status"] != "Executed successfully", "This shouldn't succeed"
-    print(json.dumps(output, indent=4, sort_keys=True))
+    if output[0]["vm_status"] == "Executed successfully":
+        print("Warning: Transaction succeeded unexpectedly. Bob's account might already exist.")
+    else:
+        print("Transaction failed as expected. Bob's account is not created yet.")
+    # print("yesss")
+    # print(json.dumps(output, indent=4, sort_keys=True))
 
     print("\n=== Simulate after creating Bob's Account ===")
-    await faucet_client.fund_account(bob.address(), 0)
+    # await faucet_client.fund_account(bob.address(), 0)
     output = await rest_client.simulate_transaction(transaction, alice)
     assert output[0]["vm_status"] == "Executed successfully", "This should succeed"
     print(json.dumps(output, indent=4, sort_keys=True))
@@ -60,4 +64,7 @@ async def main():
 
 
 if __name__ == "__main__":
+    import sys
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
